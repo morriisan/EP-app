99742b5e65d0914f08f9775895b05f82bf437018
morri <morrisan2001@gmail.com>
2025-03-11 13:58:52 +0100

    proscces chek and login signin test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 //
 // import SignIn from "@/components/sign-in";
+import SignIn from "@/components/sign-in";
 import { SignUp } from "@/components/sign-up";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+
+  }, []);
   return (
     <>
+    <div className="flex flex-col items-center justify-center h-screen">
     <SignUp />
+    <SignIn/>
+    </div>
     </>
+    
   )
 }
