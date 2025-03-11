322eeeaf15cb1e981888947817448da0bb244613
morri <morrisan2001@gmail.com>
2025-03-11 16:47:44 +0100

    yy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SignIn from "@/components/sign-in";
 import { SignUp } from "@/components/sign-up";
 
 export default function Home() {
-  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("morri");
   return (
     <>
     <div className="flex flex-col items-center justify-center h-screen">
