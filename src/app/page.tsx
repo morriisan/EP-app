9016d5fbe5500034494d2f570e67ec08e1da9fb2
morri <morrisan2001@gmail.com>
2025-03-11 13:48:03 +0100

    igen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import SignIn from "@/components/sign-in";
-//import { SignUp } from "@/components/sign-up";
+//
+// import SignIn from "@/components/sign-in";
+import { SignUp } from "@/components/sign-up";
 
 export default function Home() {
   return (
     <>
-    <SignIn />
+    <SignUp />
     </>
   )
 }
