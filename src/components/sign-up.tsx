1c712dd3b4ef2163597108221152c3afeeb89ebc
morri <morrisan2001@gmail.com>
2025-03-11 10:50:05 +0100

    igen

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 //import Image from "next/image";
-import { Loader2, X } from "lucide-react";
+import { Loader2, /* X */ } from "lucide-react";
 import { signUp } from "@/lib/auth-client";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
