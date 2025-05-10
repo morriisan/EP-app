9ecb96c832e8b31b24cdbe60309235183c101ffd
morri <morrisan2001@gmail.com>
2025-05-10 15:16:21 +0200

    build fix

--- src/app/uploader/page.tsx
+++ src/app/uploader/page.tsx
@@ -1,11 +1,15 @@
-import { MediaGalleryServer } from "@/components/media/MediaGalleryServer";
+//import { MediaGalleryServer } from "@/components/media/MediaGalleryServer";
 import { UploaderClient } from "@/components/media/UploaderClient";
+import { requireAdmin } from "@/lib/auth-utils";
 
-export default function UploaderPage() {
+export default async function UploaderPage() {
+  // This will redirect to home if not an admin
+  await requireAdmin();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UploaderClient />
-      <MediaGalleryServer isAdmin={true} />
+      
     </main>
   );
 }
