b2e028d994d9c8d81a1dde28d8737aa1c59d2184
morri <morrisan2001@gmail.com>
2025-05-23 09:28:00 +0200

    builde error

--- src/components/booking/booking-form.tsx
+++ src/components/booking/booking-form.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { toast } from 'sonner';
-import { format } from 'date-fns';
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
